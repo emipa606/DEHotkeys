a73456949848eeb67cbaa73b156a9a28a9b0a9d0
emipa606 <emil.offermo@litet.net>
2021-07-02 12:35:30 +0200

    Refactored code, updated solution

---
+++ Source/DEHotkeys/Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace DEHotkeys
+{
+    [StaticConstructorOnStartup]
+    public static class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("DEHotkeys");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
